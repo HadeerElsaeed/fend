7daf0c38f40e24636ba329ff6f9937b3ad9f33e1
HadeerElsaeed <90738357+HadeerElsaeed@users.noreply.github.com>
2022-02-17 04:52:47 +0200

    Update server.js

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,43 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = rquire("express");
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 // Cors for cross origin allowance
+const cors = require(cors);
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+//Callback function to complete Get
+const getAll = (req , res) => res.status(200).send(projectData);
+app.get("/all" , getAll);
 
+//Callback function to complete Get
+const postData = (req , res)=> 
+{
+    projectData = req.body;
+    console.log(projectData);
+    res.status(200).send(projectData);
+}
+
+//Get route
+app.post("/all" , projectData);
 // Setup Server
+const port = 8000;
+const server = app.listen(port , listening);
+function listening(){
+    console.log("server running");
+    console.log(`running on localhost ${port}`);
+}
